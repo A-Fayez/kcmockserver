60d317eb423f87c23e1b45fa86b8f47f68ad579d
Ahmed Fayez <ahmedf.higgy@gmail.com>
2023-08-07 01:37:33 +0300

    refacotor to making server instance assigned to a random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use axum::{routing::get, Router};
-use std::net::SocketAddr;
-use std::str::FromStr;
+use std::net::{SocketAddr, TcpListener};
 
 pub struct KcTestServer {
     addr: SocketAddr,
@@ -8,14 +7,16 @@ pub struct KcTestServer {
 }
 
 impl KcTestServer {
-    pub fn new() -> Self {
-        // build our application with a single route
-
+    pub async fn new() -> Self {
         let (shutdown, rx) = tokio::sync::oneshot::channel::<()>();
 
-        let addr = SocketAddr::from_str("0.0.0.0:3000").unwrap();
-        let app = Router::new().route("/", get(root));
-        let server = axum::Server::bind(&addr)
+        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        let addr = listener.local_addr().unwrap();
+        let app = Router::new()
+            .route("/", get(root))
+            .route("/slash", get(slash));
+        let server = axum::Server::from_tcp(listener)
+            .unwrap()
             .serve(app.into_make_service())
             .with_graceful_shutdown(async {
                 rx.await.ok();
@@ -29,8 +30,8 @@ impl KcTestServer {
         }
     }
 
-    pub fn endpoint(&self) -> reqwest::Url {
-        reqwest::Url::parse(&format!("http://{}", self.addr)).unwrap()
+    pub fn uri(&self, endpoint: Option<&str>) -> reqwest::Url {
+        reqwest::Url::parse(&format!("http://{}{}", self.addr, endpoint.unwrap_or("/"))).unwrap()
     }
 }
 
@@ -46,21 +47,34 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
+async fn slash() -> &'static str {
+    "Hello Slash"
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[tokio::test]
     async fn it_works() {
-        let server = KcTestServer::new();
+        let server = KcTestServer::new().await;
         println!("{}", server.addr);
         dbg!(server.addr);
-        let body = reqwest::get(server.endpoint())
+        let body = reqwest::get(server.uri(Some("/")))
             .await
             .unwrap()
             .text()
             .await
             .unwrap();
         assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn it_works_slash() {
+        let server = KcTestServer::new().await;
+        let endpoint = server.uri(Some("/slash"));
+        dbg!(server.addr);
+        let body = reqwest::get(endpoint).await.unwrap().text().await.unwrap();
+        assert_eq!(body, "Hello Slash");
     }
 }
